6353ec03a444a7c070f4ae340f06915ec66ebae6
jsf3rd <jsf3rd@9f37507a-a4b8-45bf-8f9d-a4ddc14b0577>
2014-01-27 05:57:36 +0000

    데이터 삭제 및 백업 App

---
+++ DataCleaner/DataCleaner.dpr
@@ -0,0 +1,31 @@
+program DataCleaner;
+
+uses
+  Vcl.Forms, Winapi.Windows,
+  _fmMain in '_fmMain.pas' {fmMain} ,
+  Global in 'Global.pas',
+  Option in 'Option.pas';
+
+{$R *.res}
+
+var
+  Mutex: THandle; // 뮤텍스 핸들을 하나 선언한후.
+
+begin
+  // 중복 실행 체크
+  Mutex := CreateMutex(nil, True, 'DataCleaner');
+  // noDuplicate 명이 달라야 다른프로그램으로 인식합니다. ^^
+  if (Mutex = 0) or (GetLastError <> 0) then
+  begin
+    // MessageBox(0, '프로그램이 이미 실행중입니다.', '확인', mb_IconInformation);
+    Exit;
+  end;
+  Application.Initialize;
+  Application.MainFormOnTaskbar := False;
+  Application.CreateForm(TfmMain, fmMain);
+  Application.Run;
+  // 프로그램 종료시 핸들 닫기
+  if (Mutex <> 0) then
+    CloseHandle(Mutex);
+
+end.
---
+++ DataCleaner/Global.pas
@@ -0,0 +1,82 @@
+﻿unit Global;
+
+interface
+
+uses
+  Classes, SysUtils, JdcView2, JdcGlobal, DBXJSON;
+
+const
+  COPY_RIGHT_FILE_CLEANER = 'File Cleaner v1.0 - ⓒ 2013 ENB GROUP';
+  HOME_PAGE_URL = 'http://www.enbgourp.co.kr';
+
+type
+  TGlobal = class(TComponent)
+  strict private
+    FInitialized: boolean;
+  private
+    FExeName: String;
+
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+
+    class function Obj: TGlobal;
+
+    procedure Initialize;
+    procedure Finalize;
+
+  published
+    property Initialized: boolean read FInitialized;
+    property ExeName: String read FExeName write FExeName;
+  end;
+
+implementation
+
+uses Option;
+
+var
+  MyObj: TGlobal = nil;
+
+  { TGlobal }
+
+constructor TGlobal.Create(AOwner: TComponent);
+begin
+  inherited;
+
+  FExeName := '';
+  FInitialized := false;
+end;
+
+destructor TGlobal.Destroy;
+begin
+  Finalize;
+
+  inherited;
+end;
+
+procedure TGlobal.Finalize;
+begin
+  if not FInitialized then
+    Exit;
+  FInitialized := false;
+
+end;
+
+procedure TGlobal.Initialize;
+begin
+  if FInitialized then
+    Exit;
+
+  // Todo :
+
+  FInitialized := true;
+end;
+
+class function TGlobal.Obj: TGlobal;
+begin
+  if MyObj = nil then
+    MyObj := TGlobal.Create(nil);
+  Result := MyObj;
+end;
+
+end.
