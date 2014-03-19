fdf94d860cef697d298838dc8ecb9ba5a6143cb3
jsf3rd <jsf3rd@9f37507a-a4b8-45bf-8f9d-a4ddc14b0577>
2014-03-19 08:16:05 +0000

    CR1K 라이브러리 등록

---
+++ trunk/CR1K/CR1K.dpr
@@ -0,0 +1,20 @@
+program CR1K;
+
+uses
+  madExcept,
+  madLinkDisAsm,
+  madListHardware,
+  madListProcesses,
+  madListModules,
+  Vcl.Forms,
+  _fmMain in '_fmMain.pas' {fmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfmMain, fmMain);
+  Application.Run;
+
+end.
