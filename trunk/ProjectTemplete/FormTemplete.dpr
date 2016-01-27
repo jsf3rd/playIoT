9b8f952073e3f48d6b96e183092b7a5e9e5cbd8e
jsf3rd <jsf3rd@9f37507a-a4b8-45bf-8f9d-a4ddc14b0577>
2016-01-27 02:26:31 +0000

    템플릿 수정

--- trunk/ProjectTemplete/FormTemplete.dpr
+++ trunk/ProjectTemplete/FormTemplete.dpr
@@ -2,7 +2,7 @@ program FormTemplete;
 
 uses
   Vcl.Forms,
-  Winapi.Windows,
+  // JclAppInst,  // 중복 실행 방지
   _fmMain in 'View\_fmMain.pas' {fmMain} ,
   Core in 'Core\Core.pas',
   MyCommon in 'Global\MyCommon.pas',
@@ -11,28 +11,14 @@ uses
 
 {$R *.res}
 
-var
-  Mutex: THandle;
-
 begin
-  // 중복 실행 체크
-  Mutex := CreateMutex(nil, True, APPLICATION_TITLE);
-  // APPLICATION_TITLE 명이 달라야 다른프로그램으로 인식합니다. ^^
-  if (Mutex = 0) or (GetLastError <> 0) then
-  begin
-    // MessageBox(0, '프로그램이 이미 실행중입니다.', '확인', mb_IconInformation);
-
-    // Exit; // 프로그램 종료.
-  end;
+  // 중복 실행을 막으려면 활성화 하시오.
+  // JclAppInstances.CheckSingleInstance;
 
   Application.Initialize;
   Application.Title := APPLICATION_TITLE;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TfmMain, fmMain);
   Application.Run;
-
-  // 프로그램 종료시 핸들 닫기
-  if (Mutex <> 0) then
-    CloseHandle(Mutex);
 
 end.
