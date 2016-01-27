66172af553fd6b7cb0bfd4cb18b52b421bce241c
jsf3rd <jsf3rd@9f37507a-a4b8-45bf-8f9d-a4ddc14b0577>
2016-01-27 02:26:31 +0000

    템플릿 수정

--- DataSnap4Basic/DataSnap.dpr
+++ DataSnap4Basic/DataSnap.dpr
@@ -2,10 +2,14 @@ program DataSnap;
 
 uses
   Vcl.Forms,
-  _fmMain in '_fmMain.pas' {fmMain},
-  _smDataLoader in 'DataSnap\_smDataLoader.pas' {smDataLoader: TDSServerModule},
-  _smDataProvider in 'DataSnap\_smDataProvider.pas' {smDataProvider: TDSServerModule},
-  ServerContainerUnit in 'DataSnap\ServerContainerUnit.pas' {ServerContainer: TDataModule},
+  // JclAppInst,  // 중복 실행 방지
+  _fmMain in '_fmMain.pas' {fmMain} ,
+  _smDataLoader
+    in 'DataSnap\_smDataLoader.pas' {smDataLoader: TDSServerModule} ,
+  _smDataProvider
+    in 'DataSnap\_smDataProvider.pas' {smDataProvider: TDSServerModule} ,
+  ServerContainerUnit
+    in 'DataSnap\ServerContainerUnit.pas' {ServerContainer: TDataModule} ,
   MyGlobal in 'Global\MyGlobal.pas',
   MyOption in 'Global\MyOption.pas',
   _fmOption in '_fmOption.pas' {fmOption};
@@ -13,6 +17,9 @@ uses
 {$R *.res}
 
 begin
+  // 중복 실행을 막으려면 활성화 하시오.
+  // JclAppInstances.CheckSingleInstance;
+
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.Title := APPLICATION_TITLE;
