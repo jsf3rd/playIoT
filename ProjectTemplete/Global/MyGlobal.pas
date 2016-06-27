e1b144506e0e1cda80cf17746ef5e002697c21b6
jsf3rd <jsf3rd@nate.com>
2016-06-27 15:02:30 +0900

    TGlobalAbstract 상속처리

--- ProjectTemplete/Global/MyGlobal.pas
+++ ProjectTemplete/Global/MyGlobal.pas
@@ -3,34 +3,27 @@ unit MyGlobal;
 interface
 
 uses
-  Classes, SysUtils, IOUtils;
+  Classes, SysUtils, IOUtils, JdcGlobal, MyCommon;
 
 const
+  APPLICATION_CODE = 'playIoT Application';
   APPLICATION_TITLE = 'palyIoT Form Application Templete';
   APPLICATION_VERSION = 'v1.0';
   COPY_RIGHT_SIGN = '¨Ï 2016 playIoT';
   HOME_PAGE_URL = 'http://www.playIoT.biz';
 
 type
-  TGlobal = class(TComponent)
-  strict private
-    FInitialized: boolean;
-  private
-    FExeName: String;
-    FLogName: string;
-    procedure SetExeName(const Value: String);
+  TGlobal = class(TGlobalAbstract)
+  strict protected
+    procedure SetExeName(const Value: String); override;
   public
-    constructor Create(AOwner: TComponent); override;
+    constructor Create; override;
     destructor Destroy; override;
 
     class function Obj: TGlobal;
 
-    procedure Initialize;
-    procedure Finalize;
-  published
-    property Initialized: boolean read FInitialized;
-    property ExeName: String read FExeName write SetExeName;
-    property LogName: string read FLogName;
+    procedure Initialize; override;
+    procedure Finalize; override;
   end;
 
 implementation
@@ -42,17 +35,20 @@ var
 
   { TGlobal }
 
-constructor TGlobal.Create(AOwner: TComponent);
+constructor TGlobal.Create;
 begin
   inherited;
 
-  FExeName := '';
-  FInitialized := false;
+  FProjectCode := PROJECT_CODE;
+  FAppCode := APPLICATION_CODE;
+
+  // TOTO : after create
 end;
 
 destructor TGlobal.Destroy;
 begin
-  Finalize;
+
+  // TOTO : before Finalize
 
   inherited;
 end;
@@ -78,7 +74,7 @@ end;
 class function TGlobal.Obj: TGlobal;
 begin
   if MyObj = nil then
-    MyObj := TGlobal.Create(nil);
+    MyObj := TGlobal.Create;
   result := MyObj;
 end;
 
