43d2527efc1c80e943d4c7c12d990d5c1e2abf85
jsf3rd <jsf3rd@nate.com>
2019-04-22 17:07:49 +0900

    파라미터 지정 방식 수정

--- SMSMan/_fmMain.pas
+++ SMSMan/_fmMain.pas
@@ -59,8 +59,11 @@ begin
       ManageMessage(RawMessage);
     except
       on E: Exception do
+      begin
         OperationLog('메시지(Message) 송신자(From) 수신자(To) 데이터가 필요합니다.' + NEXT_LINE +
-          '아스키코드 ($00) , 수신자 구분(/)');
+          '공백 (_),  줄바꿈 (@), 수신자 구분(/)');
+        OperationLog(Format('Params=%s,E=%s', [RawMessage.CommaText, E.Message]));
+      end;
     end;
 
     RawMessage.free;
@@ -84,25 +87,10 @@ var
   _SMSUser: string;
   SMSUser: TStringList;
   SMSMessage, AFrom, ATo: string;
-
-  Index: Integer;
-  Ascii, Msg: string;
-  Dec: Integer;
 begin
   SMSMessage := AMessage.strings[0];
-  // SMSMessage := SMSMessage.Replace('_', ' ', [rfReplaceAll]);
-  // SMSMessage := SMSMessage.Replace('@', NEXT_LINE, [rfReplaceAll]);
-
-  Msg := SMSMessage;
-  while pos('$', Msg) > 0 do
-  begin
-    Index := pos('$', Msg);
-    Ascii := Msg.Substring(Index, 2);
-
-    Dec := HexStrToByte(Ascii);
-    SMSMessage := SMSMessage.Replace('$' + Ascii, Char(Dec), [rfReplaceAll]);
-    Msg := Msg.Replace('$' + Ascii, '', [rfReplaceAll]);
-  end;
+  SMSMessage := SMSMessage.Replace('_', ' ', [rfReplaceAll]);
+  SMSMessage := SMSMessage.Replace('@', NEXT_LINE, [rfReplaceAll]);
 
   AFrom := AMessage.strings[1];
 
