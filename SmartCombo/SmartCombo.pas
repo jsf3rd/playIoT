27b693aa3529263ff2649f66612ac0d5c2dc8d39
jsf3rd <jsf3rd@nate.com>
2020-12-20 22:26:13 +0900

    할글 사용 관련 업데이트

--- SmartCombo/SmartCombo.pas
+++ SmartCombo/SmartCombo.pas
@@ -1,22 +1,20 @@
 unit SmartCombo;
 
 {
-  -Source
+  -Origin Source
   https://stackoverflow.com/questions/9466547/how-to-make-a-combo-box-with-fulltext-search-autocomplete-support
-
-  -Author1
-  https://stackoverflow.com/users/1870653/nikb
-  -Author2
-  https://stackoverflow.com/users/7974700/mprot
-
+  
   -Edit
   jsf3rd@nate.com
-  * add - FChar: char; for UNICODE(like hangul)
+  // @Prevents a bug in which characters are deleted when re-entering after deleting UniCode characters
+  // @Prevent bug where cursor hides behind form after Combo DropDown
+  // @Filter at the Unicode character level(default - letter level)
+  // @Prevents a bug in which Text is deleted when adding text after selection
 }
 
 interface
 
-uses stdctrls, classes, messages, controls, windows, sysutils;
+uses StdCtrls, Classes, Messages, Controls, Windows, SysUtils, StrUtils;
 
 type
   TSmartComboBox = class(TComboBox)
@@ -25,20 +23,23 @@ type
     // After InitSmartCombo is invoked, StoredItems is assigned and combo starts to behave as a smart combo.
     // If InitSmartCombo is not invoked it acts as standard TComboBox, it is safe to bulk replace all TComboBox in application with TSmartComboBox
   private
-    FChar: char;
-
+    FChar: Char; // @for UNICODE Filter
+    FIgnoreChar: boolean; // @for UNICODE Edit
     FStoredItems: TStringList;
-    dofilter: boolean;
-    storeditemindex: integer;
-    procedure FilterItems;
+    doFilter: boolean;
+    StoredItemIndex: Integer;
+
     procedure StoredItemsChange(Sender: TObject);
     procedure SetStoredItems(const Value: TStringList);
     procedure CNCommand(var AMessage: TWMCommand); message CN_COMMAND;
   protected
-    procedure KeyPress(var Key: char); override;
-    procedure CloseUp; override;
-    procedure Click; override;
+    procedure KeyPress(var Key: Char); override;
+
+    // @Prevents a bug in which characters are deleted when re-entering after deleting UniCode characters
+    procedure EditWndProc(var Message: TMessage); override;
   public
+    procedure FilterItems;
+
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
     property StoredItems: TStringList read FStoredItems write SetStoredItems;
@@ -47,78 +48,39 @@ type
 
 implementation
 
-procedure TSmartComboBox.KeyPress(var Key: char);
+procedure TSmartComboBox.KeyPress(var Key: Char);
 // combo dropdown must be done in keypress, if its done on CBN_EDITUPDATE it messes up whole message processing mumbo-jumbo
 begin
   inherited;
 
-  if dofilter and not(ord(Key) in [8, 13, 27]) then // BackSpace, Enter, ESC
+  if doFilter and not(Ord(Key) in [8, 13, 27]) then // BackSpace, Enter, ESC
   begin
     FChar := Key;
-
-    // if (items.Count <> 0) and not droppeddown then
-    SendMessage(Handle, CB_SHOWDROPDOWN, 1, 0);
-    // something matched -> dropdown combo to display results
-  end;
-
-  if dofilter and (ord(Key) = 8) then
-    FChar := Key;
-end;
-
-procedure TSmartComboBox.CloseUp;
-// ugly workaround for some wierd combobox/modified code interactions
-// var
-// x: string;
-begin
-  if dofilter then
-  begin
-    if (items.Count = 1) and (itemindex = 0) then
-      text := items[itemindex]
-    else if ((text <> '') and (itemindex <> -1) and (text <> items[itemindex])) or
-      ((text = '') and (itemindex = 0)) then
+    if not DroppedDown then
     begin
-      storeditemindex := itemindex;
-      {
-        // jsf3rd - 사용자 입력 후 콤보선택 시 버그
-        x := text;
-        itemindex := items.indexof(text);
-        if itemindex = -1 then
-        text := x;
-      }
-    end
-    else
-      storeditemindex := -1;
+      // @Prevent bug where cursor hides behind form after Combo DropDown
+      if Text = '' then
+        SendMessage(Handle, CB_SHOWDROPDOWN, 1, 0);
+    end;
   end;
-  inherited;
-end;
-
-procedure TSmartComboBox.Click;
-// ugly workaround for some weird combobox/modified code interactions
-begin
-  if dofilter then
-  begin
-    if storeditemindex <> -1 then
-      itemindex := storeditemindex;
-    storeditemindex := -1;
-  end;
-  inherited;
 end;
 
 procedure TSmartComboBox.InitSmartCombo;
 begin
   FStoredItems.OnChange := nil;
-  StoredItems.Assign(items);
+  StoredItems.Assign(Items);
   AutoComplete := False;
   FStoredItems.OnChange := StoredItemsChange;
-  dofilter := true;
-  storeditemindex := -1;
+  doFilter := True;
+  StoredItemIndex := -1;
 end;
 
 constructor TSmartComboBox.Create(AOwner: TComponent);
 begin
   inherited;
   FStoredItems := TStringList.Create;
-  dofilter := False;
+  FIgnoreChar := False;
+  doFilter := False;
 end;
 
 destructor TSmartComboBox.Destroy;
@@ -127,16 +89,50 @@ begin
   inherited;
 end;
 
+procedure TSmartComboBox.EditWndProc(var Message: TMessage);
+var
+  OldText: string;
+begin
+  case Message.Msg of
+    WM_IME_ENDCOMPOSITION:
+      begin
+        OldText := Self.Text;
+        inherited;
+        FIgnoreChar := Self.Text = OldText;
+      end;
+    WM_CHAR:
+      begin
+        FIgnoreChar := False;
+        inherited;
+      end;
+    WM_IME_CHAR:
+      begin
+        if FIgnoreChar then
+          FIgnoreChar := False
+        else
+          inherited;
+      end;
+  else
+    inherited;
+  end;
+end;
+
 procedure TSmartComboBox.CNCommand(var AMessage: TWMCommand);
 begin
   // we have to process everything from our ancestor
   inherited;
 
-  if FChar = #0 then
-    Exit;
+  if AMessage.Msg = CN_DRAWITEM then
+  begin
+    FChar := #0;
+  end;
+
+  // @Filter at the Unicode character level(default - letter level)
+  // if FChar = #0 then
+  // Exit;
 
   // if we received the CBN_EDITUPDATE notification
-  if (AMessage.NotifyCode = CBN_EDITUPDATE) and dofilter then
+  if (AMessage.NotifyCode = CBN_EDITUPDATE) and doFilter then
   begin
     // fill the items with the matches
     FilterItems;
@@ -147,29 +143,30 @@ end;
 
 procedure TSmartComboBox.FilterItems;
 var
-  I: integer;
+  I: Integer;
   Selection: TSelection;
+  xText: string;
 begin
   // store the current combo edit selection
   SendMessage(Handle, CB_GETEDITSEL, WPARAM(@Selection.StartPos), LPARAM(@Selection.EndPos));
 
   // begin with the items update
-  items.BeginUpdate;
+  Items.BeginUpdate;
   try
     // if the combo edit is not empty, then clear the items
     // and search through the FStoredItems
-    if text <> '' then
+    if Text <> '' then
     begin
       // clear all items
-      items.Clear;
+      Items.Clear;
       // iterate through all of them
       for I := 0 to FStoredItems.Count - 1 do
       begin
         // check if the current one contains the text in edit, case insensitive
-        if (Pos(uppercase(text), uppercase(FStoredItems[I])) > 0) then
+        if ContainsText(FStoredItems[I], Text) then
         begin
           // and if so, then add it to the items
-          items.Add(FStoredItems[I]);
+          Items.Add(FStoredItems[I]);
         end;
       end;
     end
@@ -177,11 +174,24 @@ begin
     begin
       // else the combo edit is empty
       // so then we'll use all what we have in the FStoredItems
-      items.Assign(FStoredItems);
+      Items.Assign(FStoredItems);
     end;
+
+    // @Prevents a bug in which Text is deleted when adding text after selection
+    if not DroppedDown then
+    begin
+      xText := Text;
+      SendMessage(Handle, CB_SHOWDROPDOWN, 1, 0);
+      if Items.Count = 0 then
+        ItemIndex := -1
+      else
+        ItemIndex := 0;
+      Text := xText;
+    end;
+
   finally
     // finish the items update
-    items.EndUpdate;
+    Items.EndUpdate;
   end;
   // and restore the last combo edit selection
 
